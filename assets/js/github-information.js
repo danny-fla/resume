9567d437438ad4894cc2d308fd05a0ac1d3e0060
danny-fla <dannyfla@live.ie>
2023-08-17 11:29:09 +0000

    add 403 error response

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -38,6 +38,9 @@ function repoInformationHTML(repos){
 
 function fetchGitHubInformation(event) {
 
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
+
     var username = $("#gh-username").val();
     if(!username) {
         $("#gh-user-data").html(`<h2> Please enter a GitHub username</h2>`)
@@ -63,7 +66,11 @@ function fetchGitHubInformation(event) {
                 $("#gh-user-data").html(
                     `<h2>No info found for user ${username}</h2>`
                 )
-            } else {
+            } else if (errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
+            } 
+            else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
                     `<h2>Error: ${errorResponse.responseJSON.message}</h2>`
@@ -72,4 +79,6 @@ function fetchGitHubInformation(event) {
         }
     )
 }
+
+$(document).ready(fetchGitHubInformation);
 
